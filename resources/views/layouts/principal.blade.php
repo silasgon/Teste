42f202f9f478a49e2a80c48de5fca915d6ea2d55
silasgon <silasidc@gmail.com>
2019-11-20 18:07:01 -0300

    erro no menu

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -13,8 +13,8 @@
             <div class="menu">
                 <ul>
                     <li><a class="{{ request()->routeIs('cliente.*') ? 'active' : '' }}" href="{{ route('cliente.index')}}">Clientes</a></li>
-                <li><a class="{{ request()->routeIs('produtos') ? 'active' : '' }}" href="{{ route('produtos' ) }}"></a>Produtos</li>
-                <li><a class="{{ request()->routeIs('departamentos') ? 'active' : '' }}" href="{{ route('departamentos') }}"></a>Departamentos</li>
+                <li><a class="{{ request()->routeIs('produtos') ? 'active' : '' }}" href="{{ route('produtos' ) }}">Produtos</a></li>
+                <li><a class="{{ request()->routeIs('departamentos') ? 'active' : '' }}" href="{{ route('departamentos') }}">Departamentos</a></li>
                 </ul> 
             </div>
         </div>
