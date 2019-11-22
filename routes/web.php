73fdc3daca6107793c91509e241049238c70dcd5
silasgon <silasidc@gmail.com>
2019-11-22 11:28:25 -0300

    A43- instalando bootstrap

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,10 @@ Route::get('opcoes/{opcao?}', function ($opcao=null) {
     return view('outras.opcoes', compact(['opcao']));
 })->name('opcoes');
 
+Route::get('bootstrap', function () {
+    return view('outras.exemplo');
+});
+
 
 
 
