db6ede903f369589d8f5726e9b36fe58a05ddf61
Silas Gon <silasidc@gmail.com>
2019-12-03 01:36:27 -0300

    Seção 8 e 9 - usando tinke e consultas

--- database/migrations/2019_07_17_102550_create_products.php
+++ database/migrations/2019_07_17_102550_create_products.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateProducts extends Migration
 {
@@ -18,7 +18,7 @@ class CreateProducts extends Migration
             $table->string('name');
             $table->integer('stock');
             $table->double('price');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2019_07_19_100257_create_departaments.php
+++ database/migrations/2019_07_19_100257_create_departaments.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-class CreateBrands extends Migration
+class CreateDepartaments extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateBrands extends Migration
      */
     public function up()
     {
-        Schema::create('brands', function (Blueprint $table) {
+        Schema::create('departments', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ class CreateBrands extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('brands');
+        Schema::dropIfExists('departments');
     }
 }
